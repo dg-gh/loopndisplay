1d056b1e6c7209313af03988a33a108a5ba321c7
Dave <52504186+dg-gh@users.noreply.github.com>
2020-09-03 20:25:47 +0200

    Update loopndisplay.h

--- src/loopndisplay.h
+++ src/loopndisplay.h
@@ -1040,6 +1040,10 @@ namespace lnd
 		{
 			glClear(GL_COLOR_BUFFER_BIT);
 		}
+		inline void clear_3d_window()
+		{
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		}
 
 		// pointer to window
 
