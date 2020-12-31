85092f9ac0c6c7e3fffba41f3c5c7d415b170174
Dave <52504186+dg-gh@users.noreply.github.com>
2020-12-31 11:01:09 +0100

    Update lnd_beamforming.cpp

--- code_examples/lnd_beamforming.cpp
+++ code_examples/lnd_beamforming.cpp
@@ -9,6 +9,7 @@ private:
 	lnd::shader_fragment beamforming_fs;
 	lnd::program beamforming_prog;
 	float phase_shift = 0.0f;
+	float phase_displacement = 0.0f;
 	float shift_X = 0.0f;
 	float shift_Y = 0.0f;
 	float scale = 1.0f;
@@ -31,34 +32,39 @@ void beamforming::setup()
 	quad[6] = min_X(); quad[7] = max_Y();
 
 	beamforming_fs.new_shader(
-		"#version 330 core																				\n"
-		"in vec2 X;																						\n"
-		"out vec4 color;																				\n"
-		"uniform vec2 params;																			\n"
-		"uniform vec2 shift;																			\n"
-		"uniform float scale;																			\n"
-		"uniform float dir;																				\n"
-		"uniform float d;																				\n"
-		"void main() {																					\n"
-		"	vec2 C = vec2(-0.5, 0.0) + 0.5 * params;													\n"
-		"	vec2 z = scale * vec2(X) + shift;															\n"
-		"	color = vec4(0.0, 0.0, 0.0, 1.0);															\n"
-		"	float dx = 0.003;																			\n"
-		"	float AMP = sin(400 * sqrt(z[0] * z[0] + z[1] * z[1]));										\n"
-		"	for (int k = 0; k < 40; ++k)																\n"
-		"	{																							\n"
-		"		AMP += sin(k * dir + 400 * sqrt((z[0] + k * dx) * (z[0] + k * dx) + z[1] * z[1]));		\n"
-		"		AMP += sin(- k * dir + 400 * sqrt((z[0] - k * dx) * (z[0] - k * dx) + z[1] * z[1]));	\n"
-		"	}																							\n"
-		"	if (z[1] < 0) { AMP = 0; }																	\n"
-		"	color[0] = 0.015 * max(0, AMP); color[2] = -0.1 * min(0, AMP);								\n"
-		"}																								\n"
+		"#version 330 core														\n"
+		"in vec2 X;																\n"
+		"out vec4 color;														\n"
+		"uniform vec2 params;													\n"
+		"uniform vec2 shift;													\n"
+		"uniform float scale;													\n"
+		"uniform float dir;														\n"
+		"uniform float disp;													\n"
+		"void main() {															\n"
+		"	vec2 C = vec2(-0.5, 0.0) + 0.5 * params;							\n"
+		"	vec2 z = scale * vec2(X) + shift;									\n"
+		"	color = vec4(0.0, 0.0, 0.0, 1.0);									\n"
+		"	float dx = 0.003;													\n"
+		"	float AMP = sin(-disp + 400 * sqrt(z[0] * z[0] + z[1] * z[1]));		\n"
+		"	for (int k = 0; k < 40; ++k)										\n"
+		"	{																	\n"
+		"		AMP += sin(-disp + k * dir + 400 * sqrt((z[0] + k * dx)			\n"
+		"			* (z[0] + k * dx) + z[1] * z[1]));							\n"
+		"		AMP += sin(-disp - k * dir + 400 * sqrt((z[0] - k * dx)			\n"
+		"			* (z[0] - k * dx) + z[1] * z[1]));							\n"
+		"	}																	\n"
+		"	if (z[1] < 0) { AMP = 0; }											\n"
+		"	color[0] = 0.015 * max(0, AMP); color[2] = -0.1 * min(0, AMP);		\n"
+		"}																		\n"
 	);
 	beamforming_prog.new_program(vertex_identity(), beamforming_fs);
 }
 
 inline void beamforming::loop(float dt)
 {
+	constexpr float pi = 3.14159265358979f;
+	constexpr float pi2 = 2.0f * 3.14159265358979f;
+
 	if (mouse_1()) { phase_shift += dt; }
 	if (mouse_2()) { phase_shift -= dt; }
 	if (key_W()) { shift_Y += scale * dt; }
@@ -67,6 +73,8 @@ inline void beamforming::loop(float dt)
 	if (key_D()) { shift_X += scale * dt; }
 	if (key_space_press()) { scale *= 0.8; }
 	if (key_ctrl_press() || key_shift_press()) { scale *= 1.25; }
+	phase_displacement += 40 * dt;
+	if (phase_displacement > pi) { phase_displacement -= pi2; }
 
 	call_display();
 }
@@ -77,6 +85,7 @@ inline void beamforming::display()
 	lnd::set_uniform_1f(beamforming_prog, "scale", scale);
 	lnd::set_uniform_2f(beamforming_prog, "shift", shift_X, shift_Y);
 	lnd::set_uniform_1f(beamforming_prog, "dir", phase_shift);
+	lnd::set_uniform_1f(beamforming_prog, "disp", phase_displacement);
 	draw_quad(quad, beamforming_prog);
 }
 
