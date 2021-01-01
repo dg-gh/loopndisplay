25e2af05dfffe231487210fd3760e4663fadc860
Dave <52504186+dg-gh@users.noreply.github.com>
2021-01-01 16:03:46 +0100

    Update loopndisplay.hpp

--- src/loopndisplay.hpp
+++ src/loopndisplay.hpp
@@ -913,6 +913,86 @@ namespace lnd
 		inline unsigned int get() const noexcept { return buffer_index; }
 		inline void bind() const { glBindTexture(GL_TEXTURE_2D, buffer_index); }
 		inline void unbind() const { glBindTexture(GL_TEXTURE_2D, 0); }
+		inline void bind(GLenum n) const
+		{
+			switch (n)
+			{
+			case 0: glActiveTexture(GL_TEXTURE0); break;
+			case 1: glActiveTexture(GL_TEXTURE1); break;
+			case 2: glActiveTexture(GL_TEXTURE2); break;
+			case 3: glActiveTexture(GL_TEXTURE3); break;
+			case 4: glActiveTexture(GL_TEXTURE4); break;
+			case 5: glActiveTexture(GL_TEXTURE5); break;
+			case 6: glActiveTexture(GL_TEXTURE6); break;
+			case 7: glActiveTexture(GL_TEXTURE7); break;
+			case 8: glActiveTexture(GL_TEXTURE8); break;
+			case 9: glActiveTexture(GL_TEXTURE9); break;
+			case 10: glActiveTexture(GL_TEXTURE10); break;
+			case 11: glActiveTexture(GL_TEXTURE11); break;
+			case 12: glActiveTexture(GL_TEXTURE12); break;
+			case 13: glActiveTexture(GL_TEXTURE13); break;
+			case 14: glActiveTexture(GL_TEXTURE14); break;
+			case 15: glActiveTexture(GL_TEXTURE15); break;
+			case 16: glActiveTexture(GL_TEXTURE16); break;
+			case 17: glActiveTexture(GL_TEXTURE17); break;
+			case 18: glActiveTexture(GL_TEXTURE18); break;
+			case 19: glActiveTexture(GL_TEXTURE19); break;
+			case 20: glActiveTexture(GL_TEXTURE20); break;
+			case 21: glActiveTexture(GL_TEXTURE21); break;
+			case 22: glActiveTexture(GL_TEXTURE22); break;
+			case 23: glActiveTexture(GL_TEXTURE23); break;
+			case 24: glActiveTexture(GL_TEXTURE24); break;
+			case 25: glActiveTexture(GL_TEXTURE25); break;
+			case 26: glActiveTexture(GL_TEXTURE26); break;
+			case 27: glActiveTexture(GL_TEXTURE27); break;
+			case 28: glActiveTexture(GL_TEXTURE28); break;
+			case 29: glActiveTexture(GL_TEXTURE29); break;
+			case 30: glActiveTexture(GL_TEXTURE30); break;
+			case 31: glActiveTexture(GL_TEXTURE31); break;
+			default: break;
+			}
+			glBindTexture(GL_TEXTURE_2D, buffer_index);
+		}
+		inline void unbind(GLenum n) const
+		{
+			switch (n)
+			{
+			case 0: glActiveTexture(GL_TEXTURE0); break;
+			case 1: glActiveTexture(GL_TEXTURE1); break;
+			case 2: glActiveTexture(GL_TEXTURE2); break;
+			case 3: glActiveTexture(GL_TEXTURE3); break;
+			case 4: glActiveTexture(GL_TEXTURE4); break;
+			case 5: glActiveTexture(GL_TEXTURE5); break;
+			case 6: glActiveTexture(GL_TEXTURE6); break;
+			case 7: glActiveTexture(GL_TEXTURE7); break;
+			case 8: glActiveTexture(GL_TEXTURE8); break;
+			case 9: glActiveTexture(GL_TEXTURE9); break;
+			case 10: glActiveTexture(GL_TEXTURE10); break;
+			case 11: glActiveTexture(GL_TEXTURE11); break;
+			case 12: glActiveTexture(GL_TEXTURE12); break;
+			case 13: glActiveTexture(GL_TEXTURE13); break;
+			case 14: glActiveTexture(GL_TEXTURE14); break;
+			case 15: glActiveTexture(GL_TEXTURE15); break;
+			case 16: glActiveTexture(GL_TEXTURE16); break;
+			case 17: glActiveTexture(GL_TEXTURE17); break;
+			case 18: glActiveTexture(GL_TEXTURE18); break;
+			case 19: glActiveTexture(GL_TEXTURE19); break;
+			case 20: glActiveTexture(GL_TEXTURE20); break;
+			case 21: glActiveTexture(GL_TEXTURE21); break;
+			case 22: glActiveTexture(GL_TEXTURE22); break;
+			case 23: glActiveTexture(GL_TEXTURE23); break;
+			case 24: glActiveTexture(GL_TEXTURE24); break;
+			case 25: glActiveTexture(GL_TEXTURE25); break;
+			case 26: glActiveTexture(GL_TEXTURE26); break;
+			case 27: glActiveTexture(GL_TEXTURE27); break;
+			case 28: glActiveTexture(GL_TEXTURE28); break;
+			case 29: glActiveTexture(GL_TEXTURE29); break;
+			case 30: glActiveTexture(GL_TEXTURE30); break;
+			case 31: glActiveTexture(GL_TEXTURE31); break;
+			default: break;
+			}
+			glBindTexture(GL_TEXTURE_2D, 0);
+		}
 
 		inline void new_id(int number_of_objects)
 		{
